0fc06ad21464f5e62c4c77a0c79521eaa216be07
sj.hu <sj.hu@ctrip.com>
2019-04-02 18:39:41 +0800

    reflect util add

--- mars-x-common/src/main/java/utils/ReflectUtils.java
+++ mars-x-common/src/main/java/utils/ReflectUtils.java
@@ -7,6 +7,13 @@ import java.lang.reflect.Field;
  */
 public class ReflectUtils {
 
+    /**
+     * Get the Object's field value.
+     * @param object
+     * @param fieldType
+     * @param <T>
+     * @return
+     */
     public static <T> T get(Object object, Class<T> fieldType) {
         Field[] fields = object.getClass().getDeclaredFields();
         Field field = null;
@@ -31,6 +38,35 @@ public class ReflectUtils {
             }
         } catch (IllegalAccessException e) {
             throw new RuntimeException("Can not access the field for type: " + fieldType, e);
+        }
+    }
+
+    /**
+     * Set the Object's field value.
+     * @param object
+     * @param fieldValue
+     * @param fieldType
+     * @param <T>
+     */
+    public static <T> void set(Object object, T fieldValue, Class<T> fieldType) {
+        Field[] fields = object.getClass().getDeclaredFields();
+        Field field = null;
+        for(Field item: fields) {
+            if(item.getType() == fieldType) {
+                field = item;
+                break;
+            }
+        }
+
+        if(field == null) {
+            throw new RuntimeException("Can not find the field for type: " + fieldType);
+        }
+
+        field.setAccessible(true);
+        try {
+            field.set(object, fieldValue);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException("Can not set the value of the field for type: " + fieldType, e);
         }
     }
 
