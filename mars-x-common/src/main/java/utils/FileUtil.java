da0c0a956218dc0a1c49f4526f31166eebb07eaa
sj.hu <sj.hu@ctrip.com>
2019-11-18 20:13:40 +0800

    add util: easyExcel

---
+++ mars-x-common/src/main/java/utils/FileUtil.java
@@ -0,0 +1,60 @@
+package utils;
+
+import java.io.*;
+
+public class FileUtil {
+    public static void write(String file, String content) {
+        FileWriter fw = null;
+        try {
+            //如果文件存在，则追加内容；如果文件不存在，则创建文件
+            File f = new File(file);
+            fw = new FileWriter(f, true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        PrintWriter pw = new PrintWriter(fw);
+        pw.println(content);
+        pw.flush();
+        try {
+            fw.flush();
+            pw.close();
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void write2(String file, String conent) {
+        BufferedWriter out = null;
+        try {
+            out = new BufferedWriter(new OutputStreamWriter(
+                    new FileOutputStream(file, true)));
+            out.write(conent + "\r\n");
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                out.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void write3(String fileName, String content) {
+        try {
+            // 打开一个随机访问文件流，按读写方式
+            RandomAccessFile randomFile = new RandomAccessFile(fileName, "rw");
+            // 文件长度，字节数
+            long fileLength = randomFile.length();
+            // 将写文件指针移到文件尾。
+            randomFile.seek(fileLength);
+            randomFile.writeBytes(content + "\r\n");
+            randomFile.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ mars-x-common/src/main/java/utils/json/OrderRingRatio.java
@@ -0,0 +1,175 @@
+package utils.json;
+
+import com.alibaba.fastjson.JSON;
+import utils.StringUtils;
+
+import java.util.*;
+
+public class OrderRingRatio {
+
+    public static void main(String[] args) {
+        int count = 72;
+        List<OrderStat> yestList = new ArrayList<>();
+        List<OrderStat> todayList = new ArrayList<>();
+        for(int i=1;i<=72;i++){
+            OrderStat stat = new OrderStat();
+            stat.setMarket("All");
+            String hour = String.valueOf(i/3);
+            String minute = String.valueOf((i%3)*20);
+            stat.setDate("2019-11-17 " + hour + ":" + minute + ":00");
+            System.out.println(stat.getDate());
+            stat.setOrderNumber(new Random().nextInt(20) + 5);
+            stat.setRoomNights(new Random().nextInt(50));
+            stat.setConfirmed(new Random().nextInt(18));
+            stat.setPaymentFailed(new Random().nextInt(3));
+            stat.setCanceled(new Random().nextInt(3));
+            stat.setProcessing(new Random().nextInt(2));
+            stat.setHitRisk(new Random().nextInt(2));
+            stat.setGmv_rmb(new Random().nextInt(40000));
+            stat.setRevenue_rmb(new Random().nextInt(4000));
+            yestList.add(stat);
+        }
+
+        Calendar now = Calendar.getInstance();
+        int todayNumber = (now.get(Calendar.HOUR_OF_DAY) * 60 + now.get(Calendar.MINUTE)) / 20 + 1;
+
+        for(int i=1;i<=todayNumber;i++){
+            OrderStat stat = new OrderStat();
+            stat.setMarket("All");
+            String hour = String.valueOf(i/3);
+            String minute = String.valueOf((i%3)*20);
+            stat.setDate("2019-11-18 " + hour + ":" + minute + ":00");
+            System.out.println(stat.getDate());
+            stat.setOrderNumber(new Random().nextInt(20) + 8);
+            stat.setRoomNights(new Random().nextInt(55));
+            stat.setConfirmed(new Random().nextInt(20));
+            stat.setPaymentFailed(new Random().nextInt(4));
+            stat.setCanceled(new Random().nextInt(3));
+            stat.setProcessing(new Random().nextInt(1));
+            stat.setHitRisk(new Random().nextInt(3));
+            stat.setGmv_rmb(new Random().nextInt(50000));
+            stat.setRevenue_rmb(new Random().nextInt(5000));
+            todayList.add(stat);
+        }
+
+        String yest = JSON.toJSONString(yestList);
+        String today = JSON.toJSONString(todayList);
+        System.out.println(yest.substring(0,yest.length()-1));
+        System.out.println(",");
+        System.out.println(today.substring(1));
+
+    }
+
+    static class OrderStat{
+        private String market;
+        private String date;
+        private Integer orderNumber;
+        private Integer roomNights;
+        private Integer confirmed;
+        private Integer paymentFailed;
+        private Integer canceled;
+        private Integer processing;
+        private Integer hitRisk;
+        private Integer gmv_rmb;
+        private Integer revenue_rmb;
+        private String orderCategoryList;
+
+        public String getMarket() {
+            return market;
+        }
+
+        public void setMarket(String market) {
+            this.market = market;
+        }
+
+        public String getDate() {
+            return date;
+        }
+
+        public void setDate(String date) {
+            this.date = date;
+        }
+
+        public Integer getOrderNumber() {
+            return orderNumber;
+        }
+
+        public void setOrderNumber(Integer orderNumber) {
+            this.orderNumber = orderNumber;
+        }
+
+        public Integer getRoomNights() {
+            return roomNights;
+        }
+
+        public void setRoomNights(Integer roomNights) {
+            this.roomNights = roomNights;
+        }
+
+        public Integer getConfirmed() {
+            return confirmed;
+        }
+
+        public void setConfirmed(Integer confirmed) {
+            this.confirmed = confirmed;
+        }
+
+        public Integer getPaymentFailed() {
+            return paymentFailed;
+        }
+
+        public void setPaymentFailed(Integer paymentFailed) {
+            this.paymentFailed = paymentFailed;
+        }
+
+        public Integer getCanceled() {
+            return canceled;
+        }
+
+        public void setCanceled(Integer canceled) {
+            this.canceled = canceled;
+        }
+
+        public Integer getProcessing() {
+            return processing;
+        }
+
+        public void setProcessing(Integer processing) {
+            this.processing = processing;
+        }
+
+        public Integer getHitRisk() {
+            return hitRisk;
+        }
+
+        public void setHitRisk(Integer hitRisk) {
+            this.hitRisk = hitRisk;
+        }
+
+        public Integer getGmv_rmb() {
+            return gmv_rmb;
+        }
+
+        public void setGmv_rmb(Integer gmv_rmb) {
+            this.gmv_rmb = gmv_rmb;
+        }
+
+        public Integer getRevenue_rmb() {
+            return revenue_rmb;
+        }
+
+        public void setRevenue_rmb(Integer revenue_rmb) {
+            this.revenue_rmb = revenue_rmb;
+        }
+
+        public String getOrderCategoryList() {
+            return orderCategoryList;
+        }
+
+        public void setOrderCategoryList(String orderCategoryList) {
+            this.orderCategoryList = orderCategoryList;
+        }
+    }
+
+
+}
